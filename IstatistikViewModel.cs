f4f4de18acdcfc359bb3de30e4df5ce133f47dbd
ieavci <iavcci123@gmail.com>
2024-05-31 14:54:26 +0300

    ürünler sayfası için değişimler

---
+++ IstatistikViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace moto_stok_tracking.ViewModels
+{
+    public class IstatistikViewModel
+    {
+        public int SatisSayisi { get; set; }
+        public int UrunSayisi { get; set; }
+        public int KategoriSayisi { get; set; }
+        public int SepetSayisi { get; set; }
+        public int KullaniciSayisi { get; set; }
+    }
+}
